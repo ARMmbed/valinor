87264b176b7ff61c9c72973fa72bceac92051f48
James Crosby <James.Crosby@arm.com>
2015-02-13 17:12:00 +0000

    add pyOCD gdb server integration

--- valinor/ide_detection.py
+++ valinor/ide_detection.py
@@ -6,6 +6,10 @@
 
 import subprocess
 import logging
+import multiprocessing
+import signal
+import traceback
+import Queue
 
 from distutils.spawn import find_executable
 
@@ -60,6 +64,11 @@ def _uvision_launcher(uvision_exe):
         child.wait()
     return launch_uvision
 
+def _ignoreSignal(signum, frame):
+    logging.debug('ignoring signal %s, traceback:\n%s' % (
+        signum, ''.join(traceback.format_list(traceback.extract_stack(frame)))
+    ))
+
 def _gdb_launcher(gdb_exe):
     def launch_gdb(projectfiles, executable):
         # the "projectfiles" for gdb are really command files that we should
@@ -68,18 +77,62 @@ def _gdb_launcher(gdb_exe):
         for f in projectfiles:
             cmd += ['-x', f]
         cmd.append(executable)
+        # ignore Ctrl-C while gdb is running:
+        signal.signal(signal.SIGINT, _ignoreSignal);
         child = subprocess.Popen(cmd)
         child.wait()
     return launch_gdb
 
+def _launchPyOCDGDBServer(msg_queue):
+    logger.info('starting PyOCD gdbserver...')
+    # ignore Ctrl-C, so we don't interrupt the GDB server when the
+    # being-debugged program is being stopped:
+    signal.signal(signal.SIGINT, _ignoreSignal);
+    from pyOCD.gdbserver import GDBServer
+    from pyOCD.board import MbedBoard
+    try:
+        board_selected = MbedBoard.chooseBoard()
+        with board_selected as board:
+            gdb = GDBServer(
+                board, 3333, {
+                     'break_at_hardfault': True,
+                    'step_into_interrupt': False,
+                         'break_on_reset': False,
+                }
+            )
+            if gdb.isAlive():
+                msg_queue.put('alive')
+                while gdb.isAlive():
+                    gdb.join(timeout = 0.5)
+                    # check for a "kill" message from the parent process:
+                    try:
+                        msg = msg_queue.get(False)
+                        if msg == 'kill':
+                            gdb.stop()
+                            break
+                    except Queue.Empty:
+                        pass
+    except Exception as e:
+        if gdb != None:
+            gdb.stop()
+        raise
+    msg_queue.put('dead')
 
 def _arm_none_eabi_gdb_launcher(gdb_exe):
     gdb_launcher = _gdb_launcher(gdb_exe)
     def launch_arm_gdb(projectfiles, executable):
-        # !!! TODO: launch the pyOCD GDB server in the background, it should be
-        # possible to add pyOCD as a dependency in setup.py, too, so we'll know
-        # it's always installed
+        queue = multiprocessing.Queue()
+        p = multiprocessing.Process(target=_launchPyOCDGDBServer, args=(queue,))
+        p.start()
+        # wait for an 'alive' message from the server before starting gdb
+        # itself:
+        msg = None
+        while msg != 'alive':
+            msg = queue.get()
+            if msg == 'dead':
+                raise Exception('gdb server failed to start')
         gdb_launcher(projectfiles, executable)
+        queue.put('kill')
     return launch_arm_gdb
 
 
