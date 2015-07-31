60e972c25f93922e66486942571ca917faa4d63f
Martin Kojtal <c0170@rocketmail.com>
2015-07-31 07:48:51 +0100

    Test - rename builder import to tool

--- valinor/test/test_exporter.py
+++ valinor/test/test_exporter.py
@@ -16,7 +16,7 @@ from nose.tools import *
 from os.path import abspath
 import sys
 
-from project_generator.tools.builder import Builder
+from project_generator.tools.tool import Builder
 
 # Makes sure that exporting using generic builder will fail.
 @raises(NotImplementedError)
