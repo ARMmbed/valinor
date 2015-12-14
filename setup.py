11eaa39ad2d24e985785757ee478b4592d56168d
James Crosby <James.Crosby@arm.com>
2015-12-14 16:35:24 +0000

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "valinor",
-    version = "0.0.11",
+    version = "0.0.12",
     author = 'Martin Kojtal, James Crosby',
     author_email = "c0170@rocketmail.com, James.Crosby@arm.com",
     description = ("Generate IDE project files to debug ELF files."),
