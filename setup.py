8c4fd24a50fa45d0003517e6165bea3d5b84140f
Martin Kojtal <martin.kojtal@arm.com>
2019-05-03 14:49:05 +0100

    bump version to 1.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "valinor",
-    version = "1.1.2",
+    version = "1.1.3",
     author = 'Martin Kojtal, James Crosby',
     author_email = "c0170@rocketmail.com, James.Crosby@arm.com",
     description = ("Generate IDE project files to debug ELF files."),
